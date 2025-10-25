a2eba57db1a0960eb66885a0c6d9904d6a1319df
yeon2731 <kite2731@gmail.com>
2025-10-25 15:29:43 +0900

    test(model): test Car.move()

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,23 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import racingcar.model.Car;
+
+public class CarTest {
+    @Test
+    public void smaller_then_4() {
+        Car car = new Car("pobi");
+        car.moveTest(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    public void larger_then_4() {
+        Car car = new Car("pobi");
+        car.moveTest(5);
+        car.moveTest(5);
+        assertThat(car.getPosition()).isEqualTo(2);
+    }
+}
